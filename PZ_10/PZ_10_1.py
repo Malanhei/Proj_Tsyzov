115610d4ed79a9afa08a59bbd4546921e9cd8c6a
Malanhei <vovansvav@gmail.com>
2025-02-14 01:40:05 +0300

    Update PZ_10_1.py

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -9,35 +9,6 @@
 2. Какие книги есть во всех магазинах.
 3. Хотя бы одну книгу, которая есть не во всех магазинах.
 '''
-'''
-books = {
-    "Мансистр": ["Лермонтов", "Достоевский", "Пушкин", "Тютчев"],
-    "Домбинич": ["Толстой", "Грибоедов", "Чехов", "Пушкин"],
-    "Бумбаркет": ["Пушкин", "Достоевский", "Маяковский"],
-    "Галерея": ["Чехов", "Тютчев", "Пушкин"]
-}
-
-def all_books(books):
-    b = set()
-    for collection in books.values():
-        for book in collection:
-            b.add(book)
-    return list(b)
-
-def books_all_store(books):
-    sets = []
-    for collection in books.values():
-        sets.append(set(collection))
-    return list(set.intersection(*sets))
-
-def book(books):
-    all_books_set = all_books(books)
-    common_books = books_all_store(books)
-    for i in common_books:
-        all_books_set.remove(i)
-
-    return all_books_set
-'''
 
 manistr = ("Лермонтов", "Достоевский", "Пушкин", "Тютчев")
 dombinich = ("Толстой", "Грибоедов", "Чехов", "Пушкин")
